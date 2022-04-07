9256e7660fa2737daa656cec32ef4133ee5c4d94
Siddharth <ssankar@sfsu.edu>
2022-04-06 19:41:02 -0700

    completed milestone 1

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "CSC 317 App", name: "Siddharth Sankar" });
 });
 
 module.exports = router;
